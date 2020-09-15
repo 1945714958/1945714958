e587ccdad1fc53bd19e36b9e4e1281e0be0423b9
张小宝的哥哥 <70806530+1945714958@users.noreply.github.com>
2020-09-15 14:37:16 +0800

    Add files via upload

---
+++ 五角星.py
@@ -0,0 +1,16 @@
+import turtle as t
+t.hideturtle()
+t.pu()
+t.goto(-100,-100)
+t.pd()
+t.seth(72)
+t.fd(250)
+t.bgcolor("red")
+t.fillcolor("yellow")
+t.begin_fill()
+for i in range(5):
+    t.right(144)
+    t.fd(250)
+
+t.end_fill()
+t.done()
